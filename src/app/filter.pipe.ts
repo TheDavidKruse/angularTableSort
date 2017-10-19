936ceac585e4683e476224d3795f550fa8ee5311
TheDavidKruse <thedavidkruse@gmail.com>
2017-10-18 22:08:57 -0700

    allowed for ascending and descending filtering

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,14 +5,93 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(posts: any, term: any, filterKey: string, format: string ): any {
-    console.log('term is', term, 'key is', filterKey, 'format is', format);
+  transform(posts: Object[], term: any, filterKey: string, format: string ): any {
+    console.log('term is', typeof(term), term, 'key is', filterKey, 'format is', format);
     //check if term is undefined
-    if (term === undefined) {return posts; }
-    //return updated posts array
-    return posts.filter((post) => {
-      return post[filterKey].toString().toLowerCase().includes(term.toString().toLowerCase());
-    });
-  }
 
-}
+    switch (format) {
+      case 'asc':
+      if (term === undefined && filterKey === undefined) {
+        return posts;
+      } else if (term === undefined) {
+        return posts.sort((a, b) => {
+          if (a[filterKey] < b[filterKey]) {
+             return -1;
+            } else if (a[filterKey] > b[filterKey]) {
+              return 1;
+            }else {
+              return 0;
+            }
+        });
+      } else {
+        return posts.filter((post) => {
+          return post[filterKey].toString().toLowerCase().includes(term.toLowerCase());
+        }).sort((a, b) => {
+          if (a[filterKey] < b[filterKey]) {
+             return -1;
+            } else if (a[filterKey] > b[filterKey]) {
+              return 1;
+            }else {
+              return 0;
+            }
+        });
+      }
+
+
+      case 'desc':
+      if (term === undefined && filterKey === undefined) {
+        return posts.reverse();
+      } else if (term === undefined) {
+        return posts.sort((a, b) => {
+          if (a[filterKey] < b[filterKey]) {
+             return -1;
+            } else if (a[filterKey] > b[filterKey]) {
+              return 1;
+            }else {
+              return 0;
+            }
+        }).reverse();
+      } else {
+      return posts.filter((post) => {
+        return post[filterKey].toString().toLowerCase().includes(term.toLowerCase());
+      }).sort((a, b) => {
+          if (a[filterKey] < b[filterKey]) {
+             return -1;
+            } else if (a[filterKey] > b[filterKey]) {
+              return 1;
+            }else {
+              return 0;
+            }
+      }).reverse();
+    }
+
+
+      default:
+      if (term === undefined && filterKey === undefined) {
+        return posts;
+      } else if (term === undefined) {
+        return posts.sort((a, b) => {
+          if (a[filterKey] < b[filterKey]) {
+             return -1;
+            } else if (a[filterKey] > b[filterKey]) {
+              return 1;
+            }else {
+              return 0;
+            }
+        });
+      } else {
+        return posts.filter((post) => {
+          return post[filterKey].toString().toLowerCase().includes(term.toLowerCase());
+        }).sort((a, b) => {
+          if (a[filterKey] < b[filterKey]) {
+             return -1;
+            } else if (a[filterKey] > b[filterKey]) {
+              return 1;
+            }else {
+              return 0;
+            }
+        });
+      }
+    }
+  }
+  }
